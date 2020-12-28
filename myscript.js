4e14dfb3fd96bc6c6d39793788b341c7146a07d0
Marc Larroya <68656529+Engeniuseer@users.noreply.github.com>
2020-12-28 02:27:45 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function displayComment(text) {
+  document.getElementById("demo").style.visibility = "visible";
+  document.getElementById("demo").innerHTML = text;
+}
+            
+function stop() {
+  document.getElementById("demo").style.visibility = "hidden";
+}
+var text="I've done many projects in Python related to Data Mining, Machine Learning,<br> Visual Analytics and Deep Learning.";
+var text1="I have used Java for large scale and distributed system projects (Hadoop, Spark).";
+var text2="I have used mySQL for complex queries including joins and views.";
+var text3="Use of Matlab for statistics and image processing.";
+var text4="Mainly for computer graphics and game development (including 3D)";
+var text5="Basic knowledge of web development";
+
